cf3c8d098666cc6f1c97eec2fbc2d68e3770dcc3
ALRIER <56406907+ALRIER@users.noreply.github.com>
2021-08-11 10:33:02 -0500

    Update Rcovid.R

--- Rcovid.R
+++ Rcovid.R
@@ -10,14 +10,16 @@ pkg <- function(pkg){
   sapply(pkg, require, character.only = TRUE)
 }
 #with this code i can call al the packager i need. 
-packages <- c("tidyverse","cluster", "factoextra","NbClust","tidyr",
-              "cvms","tm","NLP","SnowballC","RColorBrewer","wordcloud",
-              "RefManageR","bibliometrix","quanteda","ggplot2",
-              "ggpubr","Factoshiny", "xtable")
+#here's all packages i usually need for this and other projects too. 
+packages <- c("tidyverse", "raster","sf" ,"ggspatial","cluster", "factoextra",
+              "NbClust","tidyr", "forecast", "semPlot", "semTools", "corrplot", "corrr",
+              "haven", "psych", "dplyr", "lavaan", "readr", "cvms","tm","NLP",
+              "SnowballC","RColorBrewer","wordcloud", "RefManageR",
+              "bibliometrix","quanteda","ggplot2", "ggpubr","Factoshiny")
 #i call my function with the packages and the orders. 
 pkg(packages)
 
-#i set up the directori in hich im goint to work 
+#i set up the directory in which im goint to work 
 setwd("/home/alrier/Documentos/bibliometrías/covid bibliometry/Covid-Bmtry-master") 
 #upload the document or dataset 
 file <- ("/home/alrier/Documentos/bibliometrías/covid bibliometry/Covid-Bmtry-master/s.bib") 
@@ -31,17 +33,11 @@ S2 <- summary(object = S1, k = 20, pause = FALSE)
 #first general plots
 plot(x = S1, k = 20, pause = FALSE) 
 
-
-
-#'''Paso los documentos a formato tibble para trabajar un filtro  
-#por años a partir del año 2018 en adelante, pero es solo para hacer 
-#un análisis más liviano, después puedo retomar el objeto M que es un  
-#DF y contiene todos los resultados'''  
+#i will set my DB to a tibble, to create a filter and work only with a lil part of the dataset.    
 #N1 <- as_tibble(M) 
 #N2<- N1 %>% filter(PY >=2018) 
-#convierto nuevamente a DF para continuar trabajando sobre mi filtro 
-#M1 <- as.data.frame(N2) 
-#agrupo y resumo resultados 
+#i set it up again into a data set but now i work with the filter (just to make the analyses faster) 
+#M1 <- as.data.frame(N2)  
 
 #To obtain the most frequent cited manuscripts: 
 CR <- citations(M, field = "article", sep = ";") 
@@ -132,7 +128,7 @@ CS <- conceptualStructure(M, field="ID", method="CA", minDegree=4,
                           documents=10) 
 
 # Conceptual Structure using keywords (method="CA") 
-
+#This is the cluster chapter, i think this is one of the most important parts of the analyse.
 t<-as.tibble(M) 
 ti<- t %>% filter(PY==2021) 
 tib<- as.data.frame(ti) 
@@ -153,6 +149,83 @@ options(width=130)
 histResults <- histNetwork(M, min.citations = 1, sep = ";") 
 # Plot a historical co-citation network 
 histPlot(histResults, n = 65, size = 4, labelsize = 4, verbose = TRUE) 
+
+
+####################################################################
+#################################S2#################################
+####################################################################
+S1 <- biblioAnalysis(M, sep = ";") 
+S2 <- summary(object = S1, k = 500, pause = FALSE)
+attach(S2)
+#En este modelo se compara el año con los articulos publidos.
+year<-AnnualProduction$`Year   `
+article<-AnnualProduction$Articles
+mod2<-glm(article~year,family= poisson(link = "log"))
+S2<-summary(mod2)
+S2
+xtable(S)
+write.table(S, file="Modelo2.csv", sep=";", row.names= T)
+#Modelo 3 -> country vs total number of citations in the database. 
+pais<-TCperCountries$Country
+TC<-as.numeric(TCperCountries$`Total Citations`)
+Freq<-as.numeric(MostProdCountries$Freq)
+mod3<-glm(TC~Freq,family=poisson(link = "log"))
+S3<-summary(mod3)
+S3
+K<-order(-mod3$fitted.values)
+K
+paisord<-pais[K]
+paisord
+##########################################################################
+################# Lineal Generalized models ########################
+##########################################################################
+summary(M)
+view(P)
+N<-M[,c(13,20,23,24,34,35)]
+P<-na.omit(N)
+P$TC<-as.numeric(P$TC)
+P$PN<-as.numeric(P$PN)
+P$VL<-as.numeric(P$VL)
+P<-na.omit(P)
+attach(P)
+mod1<-glm(TC~PY+PN+DT+VL+PY*DT,family= poisson(link = "log"))
+S<-summary(mod1)
+library(xtable)
+xtable(S)
+write.table(S, file="Modelo2.csv", sep=";", row.names= T)
+####################################################################
+###Models based on S2 which is a variable for bibliometrix###
+####################################################################
+attach(S2)
+
+year<-AnnualProduction$`Year   `
+article<-AnnualProduction$Articles
+mod2<-glm(article~year,family= poisson(link = "log"))
+S2<-summary(mod2)
+S2
+library(xtable)
+xtable(S)
+write.table(S, file="Modelo2.csv", sep=";", row.names= T)
+#MODEL3
+pais<-TCperCountries$Country
+TC<-as.numeric(TCperCountries$`Total Citations`)
+Freq<-as.numeric(MostProdCountries$Freq)
+mod3<-glm(TC~Freq,family=poisson(link = "log"))
+S3<-summary(mod3)
+S3
+K<-order(-mod3$fitted.values)
+K
+paisord<-pais[K]
+paisord
+exp(mod3$coefficients)
+library(xtable)
+xtable(S)
+write.table(S, file="Modelo2.csv", sep=";", row.names= T)
+exp(mod3$coefficients)
+library(xtable)
+xtable(S)
+write.table(S, file="Modelo2.csv", sep=";", row.names= T)
+
 
 #Top authors prod-over time 
 topAU <- authorProdOverTime(M, k = 10, graph = TRUE) 
@@ -182,231 +255,98 @@ Paises <- Paises[c(1, 3)]
 names(Paises)[1] <- "Country" 
 
 #'''esto aplica solo para revistas en español''' 
-
 #Paises$Country <- c("USA", "Taiwan", "Korea",  "Reino Unido", "Alemania", "Holanda", "Italia", "Canada", "España", "China") 
-
 '''quito algonos errores''' 
-
 Paises$Freq <- suppressWarnings(as.numeric(Paises$Freq)) 
-
 '''miro los años de producción''' 
-
 Produccion <- S2$AnnualProduction 
-
 names(Produccion)[1] <- "Year" 
-
 '''seteo la segunda columna como numerica''' 
-
 Produccion$Articles <- as.numeric(Produccion$Articles) 
-
 '''revisemos los keywords''' 
-
 key<- S2$MostRelKeywords 
-
 key <- key[c(1, 2)] 
-
 plot(key) 
-
 '''graficas y plots''' 
-
 Fig1A <- ggplot(Paises, aes(x=reorder(Country, Freq) , y=Freq)) + geom_bar(stat = "identity", fill="blue") + coord_flip() + xlab("Country") + ylab("Frequency") 
-
 Fig1B <- ggplot(Produccion, aes(x=Year , y=Articles)) + geom_bar(stat = "identity", fill="blue") + xlab("Year") + ylab("Articles") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
-
 ggarrange (Fig1A, Fig1B, labels = c("A", "B"), ncol = 2, nrow = 1) 
-
-
-
 '''graficas y plots por citas y palabras clave''' 
-
 Fig1A <- ggplot(key, aes(x=1 , y=2)) + geom_bar(stat = "identity", fill="blue") + coord_flip() + xlab("KeyWords") + ylab("Articles") 
-
 Fig1B <- ggplot(AUT, aes(x=2 , y=1)) + geom_bar(stat = "identity", fill="blue") + xlab("Paper") + ylab("TotalC") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
-
 ggarrange (Fig1A, Fig1B, labels = c("A", "B"), ncol = 2, nrow = 1) 
-
 plot(Fig1A) 
-
 #este trabajo de minería se hace sobre el total de observaciones N3. 
-
 '''Minería de datos''' 
-
 texto = Corpus(VectorSource(S2$MostRelKeywords))  
-
 '''minuscula (A!=a)''' 
-
 discurso=tm_map(texto, tolower) 
-
 '''quitamos los espacios en blanco''' 
-
 discurso =tm_map(discurso, stripWhitespace) 
-
 '''quitamos la puntuacion''' 
-
 discurso = tm_map(discurso, removePunctuation) 
-
 '''quitamos los numeros''' 
-
 discurso = tm_map(discurso, removeNumbers) 
-
-
-
-
-
 '''quitamos palabras genericas y todas las que  
-
 molesten en el resultado. PDT: hay que cambiar la lista de  
-
 palabras que es de otra bibliometría''' 
-
 discurso=tm_map(discurso, removeWords, c(stopwords("english"), "ddimer","ccovid","covid","zhang", "human", "usa", "kingdom", "korea", "institute", "national", "lee", "china", "univ", "cell", "london", "medical","coronavirus", "gene", "chen", "kong", "hong", "infectious", "animal", "wang", "diseases", "veterinary", "center", "centre", "college", "sciences", "school", "protein", "public", "control", "state", "liu", "clinical", "chinese", "van", "department", "affiliated", "united", "universit", "kim", "viral", "california", "vaccine")) 
-
-
-
 ############### DATA FRAME DE PALABRAS CON SU FRECUENCIA 
-
-
-
 '''Creamos matriz de letras''' 
-
 letras= TermDocumentMatrix(discurso) 
-
 findFreqTerms(letras, lowfreq=1) 
-
 matrix=as.matrix(letras) 
-
-
-
 '''lo ordenamos y sumamos las letras de nuestra matriz''' 
-
 vector <- sort(rowSums(matrix),decreasing=TRUE)  
-
 '''creamos la data con las palabras y su frecuencia''' 
-
 dataletras <- data.frame(word= names(vector),frequencia=vector)  
-
-
-
 ################ GRAFICAMOS FRECUENCIA DE LAS PALABRAS 
-
-
-
-barplot(dataletras[1:10,]$freq, las = 2, names.arg = dataletras[1:10,]$word, 
-        
+barplot(dataletras[1:10,]$freq, las = 2, names.arg = dataletras[1:10,]$word,         
         col ="blue", main ="Most Relevant Keywords", ylab = "Frecuencia de palabras") 
-
-
-
-
-
 ############ GRAFICAMOS LA NUBE DE PALABRAS 
-
 wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 1, 
-          
           max.words=70) 
-
 '''organizar la nube de palabras''' 
-
 wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 1, 
-          
           max.words=150, random.order=FALSE, rot.per=0.2,  
-          
           colors=brewer.pal(7, "Dark2")) 
-
-
-
 #Este trabajo de minería se hace sobre los keyWords 
-
-'''Minería de datos''' 
-
+'''Minería de datos'''
 texto = Corpus(VectorSource(key))  
-
 '''minuscula (A!=a)''' 
-
 discurso=tm_map(texto, tolower) 
-
 '''quitamos los espacios en blanco''' 
-
 discurso =tm_map(discurso, stripWhitespace) 
-
 '''quitamos la puntuacion''' 
-
 discurso = tm_map(discurso, removePunctuation) 
-
 '''quitamos los numeros''' 
-
 discurso = tm_map(discurso, removeNumbers) 
-
-
-
-
-
 '''quitamos palabras genericas y todas las que  
-
 molesten en el resultado. PDT: hay que cambiar la lista de  
-
 palabras que es de otra bibliometría''' 
-
 discurso=tm_map(discurso, removeWords, c(stopwords("english"), "zhang", "human", "usa", "kingdom", "korea", "institute", "national", "lee", "china", "univ", "cell", "london", "medical","coronavirus", "gene", "chen", "kong", "hong", "infectious", "animal", "wang", "diseases", "veterinary", "center", "centre", "college", "sciences", "school", "protein", "public", "control", "state", "liu", "clinical", "chinese", "van", "department", "affiliated", "united", "universit", "kim", "viral", "california", "vaccine")) 
-
-
-
 ############### DATA FRAME DE PALABRAS CON SU FRECUENCIA 
-
-
-
 '''Creamos matriz de letras''' 
-
 letras= TermDocumentMatrix(discurso) 
-
 findFreqTerms(letras, lowfreq=5) 
-
 matrix=as.matrix(letras) 
-
-
-
-'''lo ordenamos y sumamos las letras de nuestra matriz''' 
-
+'''i will order and summ the letters in the matrix''' 
 vector <- sort(rowSums(matrix),decreasing=TRUE)  
-
-'''creamos la data con las palabras y su frecuencia''' 
-
+'''i create the data with the frecuency''' 
 dataletras <- data.frame(word= names(vector),frequencia=vector)  
-
-
-
-################ GRAFICAMOS FRECUENCIA DE LAS PALABRAS 
-
-
-
-barplot(dataletras[1:30,]$freq, las = 2, names.arg = dataletras[1:30,]$word, 
-        
+################ word frecuency plots 
+barplot(dataletras[1:30,]$freq, las = 2, names.arg = dataletras[1:30,]$word,   
         col ="blue", main ="PALABRAS MÁS FRECUENTES", ylab = "Frecuencia de palabras") 
-
-
-
-
-
-############ GRAFICAMOS LA NUBE DE PALABRAS 
-
-wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 2, 
-          
+############ word plots 
+wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 2,          
           max.words=70) 
-
-'''organizar la nube de palabras''' 
-
-wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 5, 
-          
+'''organize my world cloud''' 
+wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 5,   
           max.words=150, random.order=FALSE, rot.per=0.2,  
-          
           colors=brewer.pal(7, "Dark2")) 
-
-
-
 ################# Lineal model regresion tools################################
 ##############################################################################
-
-#i set up the directori in hich im goint to work 
+#i set up the directory in which im goint to work 
 setwd("/home/alrier/Descargas") 
 #upload the document or dataset 
 file <- ("/home/alrier/Descargas/scopusf (1).bib") 
@@ -425,82 +365,3 @@ S<-summary(mod1)
 S
 xtable(S)
 write.table(S, file="Modelo2.csv", sep=";", row.names= T)
-####################################################################
-#################################S2#################################
-####################################################################
-S1 <- biblioAnalysis(M, sep = ";") 
-S2 <- summary(object = S1, k = 500, pause = FALSE)
-attach(S2)
-#En este modelo se compara el año con los articulos publidos.
-year<-AnnualProduction$`Year   `
-article<-AnnualProduction$Articles
-mod2<-glm(article~year,family= poisson(link = "log"))
-S2<-summary(mod2)
-S2
-xtable(S)
-write.table(S, file="Modelo2.csv", sep=";", row.names= T)
-#Modelo 3 -> country vs total number of citations in the database. 
-pais<-TCperCountries$Country
-TC<-as.numeric(TCperCountries$`Total Citations`)
-Freq<-as.numeric(MostProdCountries$Freq)
-mod3<-glm(TC~Freq,family=poisson(link = "log"))
-S3<-summary(mod3)
-S3
-K<-order(-mod3$fitted.values)
-K
-paisord<-pais[K]
-paisord
-
-
-##########################################################################
-################# Moldelos lineales generalizados ########################
-##########################################################################
-
-summary(M)
-view(P)
-N<-M[,c(13,20,23,24,34,35)]
-P<-na.omit(N)
-P$TC<-as.numeric(P$TC)
-P$PN<-as.numeric(P$PN)
-P$VL<-as.numeric(P$VL)
-P<-na.omit(P)
-attach(P)
-mod1<-glm(TC~PY+PN+DT+VL+PY*DT,family= poisson(link = "log"))
-S<-summary(mod1)
-library(xtable)
-xtable(S)
-write.table(S, file="Modelo2.csv", sep=";", row.names= T)
-####################################################################
-###Modelos sobre S2 que es una variable trabajada en bibliometrix###
-####################################################################
-attach(S2)
-
-year<-AnnualProduction$`Year   `
-article<-AnnualProduction$Articles
-mod2<-glm(article~year,family= poisson(link = "log"))
-S2<-summary(mod2)
-S2
-library(xtable)
-xtable(S)
-write.table(S, file="Modelo2.csv", sep=";", row.names= T)
-#MODELO3
-pais<-TCperCountries$Country
-TC<-as.numeric(TCperCountries$`Total Citations`)
-Freq<-as.numeric(MostProdCountries$Freq)
-mod3<-glm(TC~Freq,family=poisson(link = "log"))
-S3<-summary(mod3)
-S3
-K<-order(-mod3$fitted.values)
-K
-paisord<-pais[K]
-paisord
-exp(mod3$coefficients)
-library(xtable)
-xtable(S)
-write.table(S, file="Modelo2.csv", sep=";", row.names= T)
-exp(mod3$coefficients)
-library(xtable)
-xtable(S)
-write.table(S, file="Modelo2.csv", sep=";", row.names= T)
-
-
